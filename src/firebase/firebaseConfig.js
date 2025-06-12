4069ea557cf972e4b959b50db0b9d0c05a447362
lorenzofrozza <you@example.com>
2025-06-12 13:27:37 -0300

    login register almost ready

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,4 +1,7 @@
+// src/firebase/firebaseConfig.js
 import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -12,5 +15,7 @@ const firebaseConfig = {
 }
 
 const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
+const db = getFirestore(app)
 
-export { app }
+export { app, auth, db }
